4b7124395f268afe60f09f6d7afadf611f44a74e
Krish Goel <krishgoel3@gmail.com>
2021-07-02 21:14:10 +0530

    Added the final message

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -17,6 +17,15 @@
 <section>
 	<div class="width-restriction" style="width: 70%">
 		<h1>Volunteer Certificates</h1>
+		<p>
+			Dear Volunteers,
+			<br/>
+			Thank you for all of your incredible efforts in helping CovAID reach over 24,000 users. While we may have slowed down considerably in the past few weeks, your tenacity and alacrity in providing help during the second wave's peak will be preserved in the hearts and minds of all those we managed to assist. It truly held us in awe to watch dozens of you manage an operation at this scale (2 cities, 49 volunteers, over 600 direct resources provided) with comfort and heed. Consider this certificate a memorial to that benevolent and prolific approach, and continue to inspire those around you through your fantastic endeavours. 
+			<br/><br/>
+			Regards,
+			<br/>
+			Team CovAID
+		</p>
 		<br/><br/>
 		{#await contributorsDB}
 				<p>Contributors loading...</p>
